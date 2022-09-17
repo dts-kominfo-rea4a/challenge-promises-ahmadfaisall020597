cedae4df02816abf2748cb864692952e9bc11360
ahmadfaisall.020597 <ahmadfaisall.020597@gmail.com>
2022-09-17 17:04:38 +0700

    promise

--- promise.js
+++ promise.js
@@ -1,7 +1,28 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  const ixx = await promiseTheaterIXX();
+  const vgc = await promiseTheaterVGC();
+
+  let marah = 0;
+  let tidakMarah = 0;
+
+  ixx.forEach((value) => {
+    if(value.hasil == "marah") marah++;
+    else tidakMarah++;
+  })
+  vgc.forEach((value) => {
+    if(value.hasil == "marah") marah++;
+    else tidakMarah++;
+  })
+
+  if (emosi == 'marah') {
+    return new Promise((resolved) => resolved(marah));
+  } else {
+    return new Promise((resolve) => resolve(tidakMarah));
+  }
+};
 
 module.exports = {
   promiseOutput,
